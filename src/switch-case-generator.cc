b3521cef95cb07e3fe87976df8376ede96a7fdce
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-12-20 21:45:57 -0600

    * switch-case-generator.cc (emit_error): New.
    * switch-case-generator.cc (emit_error): New.
      (PROGNAME): Likewise.
      (usage): Likewise.
      (do_generate): Catch basic errors.

--- src/switch-case-generator.cc
+++ src/switch-case-generator.cc
@@ -4,6 +4,37 @@
 #include <string_view>
 #include <cstring>
 
+/* The program name.  */
+constexpr inline static const char *PROGNAME = "switch-case-generator";
+
+/* The file to write the output.  */
+static std::string dest { };
+
+/* Sends an error to stderr.  */
+[[noreturn]] auto emit_error(const std::string_view error_message)
+  -> void
+{
+  fprintf(stderr, "%s: %s\n", PROGNAME, error_message.data());
+  std::exit(EXIT_FAILURE);
+}
+
+/* Manual for the program.  */
+[[noreturn]] auto usage() noexcept
+  -> void
+{
+  std::printf("\
+Usage: %s OUTPUT HEXADECIMAL...\n\
+\n", PROGNAME);
+
+  std::fputs("\
+Generates a file named OUTPUT with a switch-case containing\n\
+a case for each HEXADECIMAL number. HEXADECIMAL can also be\n\
+an interval of valid hexadecimal numbers, e.g. 0388-038A.\n", stdout);
+
+  std::exit(EXIT_FAILURE);
+}
+
+/* Generates the switch-case source code.  */
 auto do_generate(const std::string_view source, std::ofstream &output) noexcept
   -> void
 {
@@ -12,11 +43,26 @@ auto do_generate(const std::string_view source, std::ofstream &output) noexcept
 
   do
   {
+    if (std::strstr(chunk, "0x") or std::strstr(chunk, "0X"))
+    {
+      output.close();
+      std::remove(dest.c_str());
+      ::emit_error("invalid number: Hexadecimal number must not be preceded by `0x' or `0X'.");
+    }
+
     /* If we find a range of hexadecimal values,  */
 
     if (const char *const hyphen { strstr(chunk, "-") })
     {
       const std::ptrdiff_t position { hyphen - chunk };
+
+      if (0 == position)
+      {
+        output.close();
+        std::remove(dest.c_str());
+        ::emit_error("invalid number: Cannot parse a negative number.");
+      }
+
       char lower[5], upper[5];
       std::uint32_t lower_ui { }, upper_ui { };
 
@@ -31,6 +77,13 @@ auto do_generate(const std::string_view source, std::ofstream &output) noexcept
 
       std::sscanf(lower, "%x", &lower_ui);
       std::sscanf(upper, "%x", &upper_ui);
+
+      if (upper_ui < lower_ui)
+      {
+        output.close();
+        std::remove(dest.c_str());
+        ::emit_error("illegal range: Lower bound cannot be greater than upper bound.");        
+      }
 
       while (lower_ui <= upper_ui)
       {
@@ -53,24 +106,29 @@ auto do_generate(const std::string_view source, std::ofstream &output) noexcept
   output.close();
 }
 
-auto main([[maybe_unused]] std::int32_t argc, [[maybe_unused]] char **argv) -> std::int32_t
+auto main([[maybe_unused]] std::int32_t argc, [[maybe_unused]] char **argv)
+  -> std::int32_t
 {
-  const std::string output_directory { "out" };
-
-  if (!std::filesystem::exists(output_directory))
+  if (argc < 3)
   {
-    std::filesystem::create_directory(output_directory);
+    ::usage();
   }
 
-  std::filesystem::path output_path{output_directory};
-  std::ofstream out_stream { output_path.c_str() };
-  const std::string fallback_output_file_name { "default_output.cpp" };
+  std::string input;
+  char **begin = argv + 1;
+  dest = argv[1];
 
-  output_path /= fallback_output_file_name;
-  std::string default_input { "31 0386 0388-038a 038c 038e-03a1 03a3-03ce 03d0-03d6 03da 03dc 03de 03e0 32 03e2-03f3 1f00-1f15 1f18-1f1d 1f20-1f45 1f48-1f4d 1f50-1f57 1f59 1f5b 33 1f5d 1f5f-1f7d 1f80-1fb4 1fb6-1fbc 1fc2-1fc4 1fc6-1fcc 1fd0-1fd3 34 1fd6-1fdb 1fe0-1fec 1ff2-1ff4 1ff6-1ffc" };
-  do_generate(default_input, out_stream);
-  
+  while (++begin < argv + argc)
+  {
+    /* FIXME: Avoid using concatenation.  */
+
+    input += *begin;
+    input += " ";
+  }
+
+  std::ofstream out_stream { dest };
+  do_generate(input, out_stream);
   out_stream.close();
-  
+
   return EXIT_SUCCESS;
 }
